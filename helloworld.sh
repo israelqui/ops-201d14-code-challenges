f5d69c9d6bfca6fa0ce8149031ac91971a4d7762
israelqui <quirolaisraela@gmail.com>
2023-10-25 22:15:34 -0400

    this is my first challenge

---
+++ helloworld.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script Name:                  helloworld.sh
+# Author:                       Israel Quirola
+# Date of latest revision:      10/25/2023
+# Purpose:                      print hello world
+# Execution                     bash  helloworld.sh 
+
+# Declaration of variables
+message="I am working perfectly!"
+
+# Declaration of functions
+
+# Main
+
+echo "Hello world"
+echo $message
+
+# End
