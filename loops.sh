11e0d0524abeca2b2df7bf2eb637b5efefdfb5fd
israelqui <quirolaisraela@gmail.com>
2023-10-30 22:28:46 -0400

    updated challenge

--- loops.sh
+++ loops.sh
@@ -2,7 +2,7 @@
 #
 # Script Name:                  loops.sh
 # Author Name:                  Israel QuirolaSalas
-# Date of latest revision:      10/25/2023
+# Date of latest revision:      10/30/2023
 # purpose:                      demo loops
 # Execution:                    bash loops.sh or ./loops.sh chmod -x loops.sh
 # Additional Sources:           x
@@ -13,13 +13,26 @@ var=0
 
 # Declarations of functions
 
-# Main
-while [ $var -lt 15 ]
+while [ $var -lt 9 ]
 
 do  
     echo $var
     var=$((var+1))
 done
+
+kill_process (){
+
+    while :
+    do
+        ps aux
+        read -p "Enter PID: " process_id
+        kill -9 $process_id
+    done
+}
+
+# Main
+kill_process
+
 
 
 # End
